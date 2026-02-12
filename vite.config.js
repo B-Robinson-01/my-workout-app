bbc75fc0efdadbd3a1a8ce3799c86efc3a3a1964
BRobinson <b.robinson.cse@gmail.com>
2026-02-11 17:31:01 -0800

    [Feature] Configure Repo Hosting Files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://b-robinson-01.github.io/my-workout-app/"
 })
